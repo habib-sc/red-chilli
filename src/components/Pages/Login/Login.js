2d3b2279a76e3fbb4d7e21db2f9d7c9f9e1ded3a
habib-sc <habib97.cs@gmail.com>
2022-04-16 20:23:06 +0600

    Login-register form real time form validation

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -12,6 +12,12 @@ const Login = () => {
         password: ''
     });
 
+    const [errors, setErrors] = useState({
+        emailError: '',
+        passwordError: '',
+        hookError: ''
+    });
+
     const navigate = useNavigate();
 
     const [
@@ -22,10 +28,26 @@ const Login = () => {
     ] = useSignInWithEmailAndPassword(auth);
 
     const handleEmail = e => {
-        setUserInfo({ ...userInfo, email: e.target.value });
+        const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        const validEmail = emailRegEx.test(e.target.value);
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value });
+            setErrors({...errors, emailError: ''})
+        }else{
+            setErrors({...errors, emailError: 'Invalid Email!'});
+        }
     }
     const handlePassword = e => {
-        setUserInfo({ ...userInfo, password: e.target.value });
+        const passwordRegEx = /.{6,}/;
+        const validPassword = passwordRegEx.test(e.target.value);
+
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: e.target.value });
+            setErrors({...errors, passwordError: ''});
+        }else{
+            setErrors({...errors, passwordError: 'At least 6 character required!'})
+        }
     }
 
     // Handling Sign up form on submit 
@@ -44,7 +66,13 @@ const Login = () => {
             </div>
             <form onSubmit={handleLoginForm} className='d-flex flex-column w-25 mx-auto mt-4'>
                 <input onChange={handleEmail} type="email" name='email' placeholder='Email' className='rc-form-input my-2 border-0 rounded-3' />
+                {errors.emailError && 
+                <p className='rc-input-error'>{errors.emailError}</p>
+                }
                 <input onChange={handlePassword} type="password" name='password' placeholder='Password' className='rc-form-input my-2 border-0 rounded-3' />
+                {errors.passwordError && 
+                <p className='rc-input-error'>{errors.passwordError}</p>
+                }
                 <button className='btn btn-danger mb-5 mt-2'>Login</button>
             </form>
         </div>
--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import logo from '../../../assets/images/red-chilli-logo.png';
 import auth from '../../../firebase.init';
 import './Register.css';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Register = () => {
     // User Information hook 
@@ -11,6 +11,12 @@ const Register = () => {
         email: '',
         phone: '',
         password: ''
+    });
+
+    const [errors, setErrors] = useState({
+        emailError: '',
+        passwordError: '',
+        hookError: ''
     });
 
     // Create user hook - React Firebase Hooks 
@@ -29,10 +35,27 @@ const Register = () => {
         setUserInfo({ ...userInfo, phone: e.target.value });
     }
     const handleEmail = e => {
-        setUserInfo({ ...userInfo, email: e.target.value });
+        const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        const validEmail = emailRegEx.test(e.target.value);
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value });
+            setErrors({...errors, emailError: ''})
+        }else{
+            setErrors({...errors, emailError: 'Invalid Email!'});
+        }
     }
+
     const handlePassword = e => {
-        setUserInfo({ ...userInfo, password: e.target.value });
+        const passwordRegEx = /.{6,}/;
+        const validPassword = passwordRegEx.test(e.target.value);
+
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: e.target.value });
+            setErrors({...errors, passwordError: ''});
+        }else{
+            setErrors({...errors, passwordError: 'At least 6 character required!'})
+        }
     }
     const handleConfirmPassword = e => {
         setUserInfo({ ...userInfo, password: e.target.value });
@@ -56,8 +79,14 @@ const Register = () => {
             <form onSubmit={handleSignUpForm} className='d-flex flex-column w-25 mx-auto mt-4'>
                 <input onChange={handleName} type="text" name='name' placeholder='Name' className='rc-form-input my-2 border-0 rounded-3' />
                 <input onChange={handleEmail} type="email" name='email' placeholder='Email' className='rc-form-input my-2 border-0 rounded-3' />
+                {errors.emailError && 
+                <p className='rc-input-error'>{errors.emailError}</p>
+                }
                 <input onChange={handlePhone} type="phone" name='phone' placeholder='Phone' className='rc-form-input my-2 border-0 rounded-3' />
                 <input onChange={handlePassword} type="password" name='password' placeholder='Password' className='rc-form-input my-2 border-0 rounded-3' />
+                {errors.passwordError && 
+                <p className='rc-input-error'>{errors.passwordError}</p>
+                }
                 <input onChange={handleConfirmPassword} type="password" name='confirm-password' placeholder='Confirm Password' className='rc-form-input my-2 border-0 rounded-3' />
                 <button onChange={handleName} className='btn btn-danger mb-5 mt-2'>Sign Up</button>
             </form>
