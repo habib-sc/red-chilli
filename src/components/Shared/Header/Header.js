9427ba61263d54e3c6bed5389852e5ddc11ab75d
habib-sc <habib97.cs@gmail.com>
2022-04-16 18:51:15 +0600

    added logout feature

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/images/red-chilli-logo.png';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
+    
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
@@ -18,7 +26,11 @@ const Header = () => {
                                 <Link className='nav-link' to='/login'>Login</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className='nav-link' to='/register'><button className='btn btn-danger rounded-pill'>Sign Up</button></Link>
+                                {user?
+                                    <Nav.Link onClick={handleSignOut} className='nav-link'>Logout</Nav.Link>
+                                    :
+                                    <Link className='nav-link' to='/register'><button className='btn btn-danger rounded-pill'>Sign Up</button></Link>
+                                }
                             </li>
                         </Nav>
                     </Navbar.Collapse>
