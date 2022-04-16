d1eb3f168d8786c5a648bade8ff7dd201dfe8413
Habibur Rahman <habib97.cs@gmail.com>
2022-04-16 16:57:33 +0600

    Intigrate login-register with firebase auth & react firebase hooks and setup custom env

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import logo from '../../../assets/images/red-chilli-logo.png';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+
+    // User Information hook 
+    const [userInfo, setUserInfo] = useState({
+        email: '',
+        password: ''
+    });
+
+    const navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmail = e => {
+        setUserInfo({ ...userInfo, email: e.target.value });
+    }
+    const handlePassword = e => {
+        setUserInfo({ ...userInfo, password: e.target.value });
+    }
+
+    // Handling Sign up form on submit 
+    const handleLoginForm = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(userInfo.email, userInfo.password);
+        navigate('/');
+    }
+
     return (
         <div>
             <div className='text-center mt-3'>
@@ -9,9 +42,9 @@ const Login = () => {
                 <hr style={{ 'width': '250px' }} className='mx-auto mt-1' />
                 <h2>Login</h2>
             </div>
-            <form className='d-flex flex-column w-25 mx-auto mt-4'>
-                <input type="email" name='email' placeholder='Email' className='rc-form-input my-2 border-0 rounded-3' />
-                <input type="password" name='password' placeholder='Password' className='rc-form-input my-2 border-0 rounded-3' />
+            <form onSubmit={handleLoginForm} className='d-flex flex-column w-25 mx-auto mt-4'>
+                <input onChange={handleEmail} type="email" name='email' placeholder='Email' className='rc-form-input my-2 border-0 rounded-3' />
+                <input onChange={handlePassword} type="password" name='password' placeholder='Password' className='rc-form-input my-2 border-0 rounded-3' />
                 <button className='btn btn-danger mb-5 mt-2'>Login</button>
             </form>
         </div>
--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../../assets/images/red-chilli-logo.png';
+import auth from '../../../firebase.init';
 import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Register = () => {
+    // User Information hook 
+    const [userInfo, setUserInfo] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        password: ''
+    });
+
+    // Create user hook - React Firebase Hooks 
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    // Handling Inputs 
+    const handleName = e => {
+        setUserInfo({ ...userInfo, name: e.target.value });
+    }
+    const handlePhone = e => {
+        setUserInfo({ ...userInfo, phone: e.target.value });
+    }
+    const handleEmail = e => {
+        setUserInfo({ ...userInfo, email: e.target.value });
+    }
+    const handlePassword = e => {
+        setUserInfo({ ...userInfo, password: e.target.value });
+    }
+    const handleConfirmPassword = e => {
+        setUserInfo({ ...userInfo, password: e.target.value });
+    }
+    // Handling Sign up form on submit 
+    const handleSignUpForm = e => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(userInfo.email, userInfo.password);
+        if (user) {
+            console.log('successfully registered');
+        }
+    }
+
     return (
         <div>
             <div className='text-center mt-3'>
@@ -10,13 +53,13 @@ const Register = () => {
                 <hr style={{ 'width': '250px' }} className='mx-auto mt-1' />
                 <h2>Sign Up</h2>
             </div>
-            <form className='d-flex flex-column w-25 mx-auto mt-4'>
-                <input type="text" name='name' placeholder='Name' className='rc-form-input my-2 border-0 rounded-3' />
-                <input type="email" name='email' placeholder='Email' className='rc-form-input my-2 border-0 rounded-3' />
-                <input type="phone" name='phone' placeholder='Phone' className='rc-form-input my-2 border-0 rounded-3' />
-                <input type="password" name='password' placeholder='Password' className='rc-form-input my-2 border-0 rounded-3' />
-                <input type="password" name='confirm-password' placeholder='Confirm Password' className='rc-form-input my-2 border-0 rounded-3' />
-                <button className='btn btn-danger mb-5 mt-2'>Sign Up</button>
+            <form onSubmit={handleSignUpForm} className='d-flex flex-column w-25 mx-auto mt-4'>
+                <input onChange={handleName} type="text" name='name' placeholder='Name' className='rc-form-input my-2 border-0 rounded-3' />
+                <input onChange={handleEmail} type="email" name='email' placeholder='Email' className='rc-form-input my-2 border-0 rounded-3' />
+                <input onChange={handlePhone} type="phone" name='phone' placeholder='Phone' className='rc-form-input my-2 border-0 rounded-3' />
+                <input onChange={handlePassword} type="password" name='password' placeholder='Password' className='rc-form-input my-2 border-0 rounded-3' />
+                <input onChange={handleConfirmPassword} type="password" name='confirm-password' placeholder='Confirm Password' className='rc-form-input my-2 border-0 rounded-3' />
+                <button onChange={handleName} className='btn btn-danger mb-5 mt-2'>Sign Up</button>
             </form>
         </div>
     );
